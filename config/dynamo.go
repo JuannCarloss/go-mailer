762b9a85a40fa9bcd98c8397ca4bd2fb6dd106ce
JuannCarloss <juancarlosbatistazeferino@gmail.com>
2024-07-21 18:50:16 -0300

    feat: create table config for dynamoDB

---
+++ config/dynamo.go
@@ -0,0 +1,70 @@
+package config
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+)
+
+var (
+	sess      = NewSession()
+	svc       = dynamodb.New(sess)
+	tableName = "emails"
+)
+
+func CreateTableEmails() {
+	input := &dynamodb.DescribeTableInput{
+		TableName: aws.String(tableName),
+	}
+
+	_, err := svc.DescribeTable(input)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok && aerr.Code() == dynamodb.ErrCodeResourceNotFoundException {
+			createTable()
+		} else {
+			log.Fatalf("Error describing table: %v", err)
+		}
+	} else {
+		fmt.Println("Table", tableName, "already exists")
+	}
+}
+
+func createTable() {
+	input := &dynamodb.CreateTableInput{
+		AttributeDefinitions: []*dynamodb.AttributeDefinition{
+			{
+				AttributeName: aws.String("To"),
+				AttributeType: aws.String("S"),
+			},
+			{
+				AttributeName: aws.String("Timestamp"),
+				AttributeType: aws.String("S"),
+			},
+		},
+		KeySchema: []*dynamodb.KeySchemaElement{
+			{
+				AttributeName: aws.String("To"),
+				KeyType:       aws.String("HASH"),
+			},
+			{
+				AttributeName: aws.String("Timestamp"),
+				KeyType:       aws.String("RANGE"),
+			},
+		},
+		ProvisionedThroughput: &dynamodb.ProvisionedThroughput{
+			ReadCapacityUnits:  aws.Int64(10),
+			WriteCapacityUnits: aws.Int64(10),
+		},
+		TableName: aws.String(tableName),
+	}
+
+	_, err := svc.CreateTable(input)
+	if err != nil {
+		log.Fatalf("Got error calling CreateTable: %s", err)
+	}
+
+	fmt.Println("Created the table", tableName)
+}
